c9f44e9b696a67caf8b7fd258c62c52737984bc7
kita <matometaru@gmail.com>
2023-07-17 18:21:44 +0900

    deploy:functions

--- packages/common/index.ts
+++ packages/common/index.ts
@@ -1,6 +1,6 @@
 import { format } from 'date-fns'
 
-export const name = 'common123'
+export const name = 'common12'
 export function formatDate(date: Date) {
   return format(date, 'yyyy-MM-dd HH:mm')
 }
