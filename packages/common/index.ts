10926c5feec1edf12f6e14147c10a6736e9a74ae
kita <matometaru@gmail.com>
2023-07-17 17:41:24 +0900

    common読み込み & エミュレーター起動まで

--- packages/common/index.ts
+++ packages/common/index.ts
@@ -1,5 +1,6 @@
 import { format } from 'date-fns'
 
+export const name = 'common123'
 export function formatDate(date: Date) {
   return format(date, 'yyyy-MM-dd HH:mm')
 }
--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from "firebase-functions";
+import { name, formatDate } from "common"
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  response.send("Hello from Firebase!" + name + ', ' + formatDate(new Date()));
 });
